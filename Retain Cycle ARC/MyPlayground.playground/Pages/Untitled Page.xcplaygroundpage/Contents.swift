13f3754a2883c2caa6e265ad8b9e902eb1bc9c46
Samim Hakimi <30713537+sammyHa@users.noreply.github.com>
2022-04-26 10:59:55 -0400

    Update Contents.swift

--- Retain Cycle ARC/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Retain Cycle ARC/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,39 +1,4 @@
 import UIKit
-
-/* pascals triangle
-       [1]
-     [1, 1]
-    [1, 2, 1]
-   [1, 3, 3, 1]
- [1, 4, 6, 4, 1]
- 
- */
-func solve(_ numRows: Int) -> [[Int]] {
-    guard numRows > 0 else {
-        return []
-    }
-    if numRows == 1 {return [[1]]}
-    
-    var result = [[Int]]()
-    result.reserveCapacity(numRows)
-    result.append([1])
-    
-    for x in 1..<numRows {
-        var newRow = [1]
-        let prevRow = result[x - 1]
-        
-        for j in 1..<prevRow.count {
-            let sum = prevRow[j] + prevRow[j - 1]
-            newRow.append(sum)
-        }
-        newRow.append(1)
-        result.append(newRow)
-    }
-    return result
-}
-
-
-
 /**
  Retain Cycle
  */
